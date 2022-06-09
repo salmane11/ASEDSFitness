2d79fc8a2dacc89ff12ee51d2500c0a1d54e94c8
jamal <jmlaitsalh@gmail.com>
2022-06-09 05:41:03 +0100

    add some code to intent

--- app/src/main/java/ma/ac/inpt/asedsfitness2/Description.java
+++ app/src/main/java/ma/ac/inpt/asedsfitness2/Description.java
@@ -2,6 +2,8 @@ package ma.ac.inpt.asedsfitness2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Description extends AppCompatActivity {
     TextView textView, textView1;
     ImageView imageView1,imageView2;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +22,21 @@ public class Description extends AppCompatActivity {
         textView1= findViewById(R.id.textViewTitleOfExercise);
         imageView1 = findViewById(R.id.imageViewExDesc1);
         imageView2 = findViewById(R.id.imageViewExDesc2);
+        button = findViewById(R.id.buttonAddExercise);
         Intent intent = getIntent();
         ExerciseObject object = (ExerciseObject) intent.getSerializableExtra("ex");
         textView.setText(object.description);
         textView1.setText(object.title);
         imageView1.setImageResource(object.descImage1);
         imageView2.setImageResource(object.descImage2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent();
+                if (object!=null) intent1.putExtra("obj",object);
+                startActivity(intent1);
+            }
+        });
 
     }
 }
