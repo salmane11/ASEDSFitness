5e015501a8a1e4e68464604d8b27ba7305a36774
Najwa983 <Najwa.lakmouri1@gmail.com>
2022-06-09 02:37:51 +0100

    switching between description and tracking activities

--- app/src/main/java/ma/ac/inpt/asedsfitness2/Description.java
+++ app/src/main/java/ma/ac/inpt/asedsfitness2/Description.java
@@ -2,6 +2,8 @@ package ma.ac.inpt.asedsfitness2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,20 +13,37 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Description extends AppCompatActivity {
     TextView textView, textView1;
     ImageView imageView1,imageView2;
+    Button btn;
+    ExerciseObject object;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_description);
+        setContentView(R.layout.activity_deescription);
         textView = findViewById(R.id.textViewExerciseDescription);
         textView1= findViewById(R.id.textViewTitleOfExercise);
         imageView1 = findViewById(R.id.imageViewExDesc1);
         imageView2 = findViewById(R.id.imageViewExDesc2);
+        btn=(Button)findViewById(R.id.button4);
         Intent intent = getIntent();
-        ExerciseObject object = (ExerciseObject) intent.getSerializableExtra("ex");
+        object = (ExerciseObject) intent.getSerializableExtra("ex");
         textView.setText(object.description);
         textView1.setText(object.title);
         imageView1.setImageResource(object.descImage1);
         imageView2.setImageResource(object.descImage2);
 
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Description.this, tracking.class));
+            }
+        });
+
+
+
+    }
+    public void addExercice() {
+        Intent intent = new Intent(Description.this,tracking.class);
+        startActivity(intent);
     }
 }
